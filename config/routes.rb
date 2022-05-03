e017ad996191d36d56d85c96464fddee87eea8c6
Blessing Ubogu <blessingubogu@Blessings-MacBook-Air.local>
2022-05-03 22:15:25 +0100

    redirected the route for the user regsiter link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    devise_scope :user do 
+      root "devise/registrations#edit", as: :authenticated_root
+    end
+  end
+
+  devise_scope :user do 
+    root "devise/sessions#new"
+  end
   devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  
 end
