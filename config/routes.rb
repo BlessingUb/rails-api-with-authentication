fa3149cd17fc96d37fa2850bf71ed67d4963ca5e
Blessing Ubogu <blessingubogu@Blessings-MacBook-Air.local>
2022-05-04 12:13:48 +0100

    routes for posts updated and migration

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,14 @@ Rails.application.routes.draw do
   namespace :user do
     resource :private_api_keys, only: :update
   end
+
+  namespace :api do
+    namespace :v1 do
+      defaults format: :json do
+        resources :posts, only: [:index, :create, :show, :update, :destroy]
+      end
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
